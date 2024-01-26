93cb3ac280122b0dddb786bbd53896f8003f2051
ira089 <gvir2015@gmail.com>
2024-01-26 23:55:52 +0200

    hw05_movies_02

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,15 +1,20 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// async function searchImg(search, page) {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const API_KEY = '9e2a62ec3e9bdb773f5bbc677529d929';
+async function tredingMoviesApi() {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+//   const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTJhNjJlYzNlOWJkYjc3M2Y1YmJjNjc3NTI5ZDkyOSIsInN1YiI6IjY1YjNmNjFiMDAyMTM0MDE2NDdjMjMzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.epkZynBE78-YD7KROr6bHdZoaxNDS6zSxB5Q7s92oo8';
 
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTJhNjJlYzNlOWJkYjc3M2Y1YmJjNjc3NTI5ZDkyOSIsInN1YiI6IjY1YjNmNjFiMDAyMTM0MDE2NDdjMjMzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.epkZynBE78-YD7KROr6bHdZoaxNDS6zSxB5Q7s92oo8
 
-//   const { data } = await axios.get(
-//     `${BASE_URL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`
-//   );
-//   console.log(data);
-//   return data;
-// }
-// export default searchImg;
+  const { data } = await axios.get(
+    `${BASE_URL}trending/movie/day?language=en-US`, {
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTJhNjJlYzNlOWJkYjc3M2Y1YmJjNjc3NTI5ZDkyOSIsInN1YiI6IjY1YjNmNjFiMDAyMTM0MDE2NDdjMjMzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.epkZynBE78-YD7KROr6bHdZoaxNDS6zSxB5Q7s92oo8',
+            'accept': 'application/json'
+        }
+    }
+  );
+  console.log(data);
+  return data;
+}
+export default tredingMoviesApi;
